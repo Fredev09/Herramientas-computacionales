296705e5b4638e59eab3acbc371688730952410c
Fredev7/ <elfreigod@gmail.com>
2026-04-17 13:06:23 -0500

    Finish2.0

--- Herramientas Comp. Interp. Val Result/Exposicion/app.py
+++ Herramientas Comp. Interp. Val Result/Exposicion/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-import mysql.connector
 import estudiantes
 
 app = Flask(__name__)
---
+++ Herramientas Comp. Interp. Val Result/Exposicion/prueba.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE prueba;
+USE prueba;
+
+CREATE TABLE estudiantes (
+id INT auto_increment primary key,
+nombre VARCHAR(100),
+edad INT
+);
